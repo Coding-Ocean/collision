13e452859714d2d9c8237c83c2b074d7fa573c27
Coding_Ocean <eijo524@gmail.com>
2022-05-13 11:34:07 +0900

    プロジェクト ファイルを追加します。

---
+++ appOneLT/gmain.cpp
@@ -0,0 +1,11 @@
+#include"libOne.h"
+void gmain()
+{
+	window(1000, 1000);
+	
+	//ƒƒCƒ“ƒ‹[ƒv
+	while (notQuit)
+	{
+		clear(128);
+	}
+}
---
+++ libOneLT/inc/MATRIX.h
@@ -0,0 +1,33 @@
+#pragma once
+#include"VECTOR.h"
+
+class MATRIX {
+public:
+    float   _11, _12, _13, _14,
+            _21, _22, _23, _24,
+            _31, _32, _33, _34,
+            _41, _42, _43, _44;
+    //s—ñ‚ğ‚Â‚­‚é
+    //MATRIX();
+    void identity();
+    void translate(float x, float y, float z = 0);
+    void translate(class VECTOR& t);
+    void scaling(float x, float y, float z=1);
+    void rotateZ(float angle);
+    void rotateY(float angle);
+    void rotateX(float angle);
+    void camera(const VECTOR& camPos, const VECTOR& lookatPos, const VECTOR& upVec);
+    void pers(float angle, float aspect, float n, float f);
+    void ortho(float l, float r, float b, float t, float n, float f);
+    //s—ñ“¯m‚ÌŠ|‚¯Z
+    void mulTranslate(float x, float y, float z = 0);
+    void mulTranslate(const VECTOR& t);
+    void mulScaling(float x, float y, float z = 1);
+    void mulRotateZ(float angle);
+    void mulRotateY(float angle);
+    void mulRotateX(float angle);
+    void mulRotateYXZ(const VECTOR& r);
+    MATRIX operator*(const MATRIX& m) const;
+    //s—ñ‚ÆƒxƒNƒgƒ‹‚ÌŠ|‚¯Z
+    VECTOR operator*(const VECTOR& v) const;
+};
