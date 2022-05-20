31eb6f1dcf30cd7604af45d8b917af2202813ccb
Coding-Ocean <eijo524@gmail.com>
2022-05-20 20:59:37 +0900

    add color

--- appOneLT/gmain.cpp
+++ appOneLT/gmain.cpp
@@ -33,15 +33,15 @@ void gmain()
 {
 	window(1920, 1080, full);
 	//線分
-	VECTOR oa(0, 0.25f);
-	VECTOR ob(0, -0.25f);
-	VECTOR a, b;
+	VECTOR osp(0, 0.25f);
+	VECTOR oep(0, -0.25f);
+	VECTOR sp, ep;
 	VECTOR sTran(0.5f,0.5f), sRot;
 	//直線と垂線
-	VECTOR oc(1, 0);
-	VECTOR od(-1, 0);
+	VECTOR op0(1, 0);
+	VECTOR op1(-1, 0);
 	VECTOR ovn(0, 1);
-	VECTOR c, d, vn;
+	VECTOR p0, p1, vn;
 	VECTOR lTran, lRot;
 	//交点の座標
 	VECTOR ip;
@@ -54,6 +54,10 @@ void gmain()
 	int dispSegSw = 0;
 	int operateObjSw = 0;
 
+	COLOR white(210, 210, 210);
+	COLOR pink(255, 128, 128);
+	COLOR green(0, 255, 0);
+	COLOR yellow(255, 255, 0);
 	COLOR lineColor;
 	COLOR gray(128, 128, 128);
 	COLOR red(255,0,0);
@@ -72,30 +76,30 @@ void gmain()
 		}
 		world.translate(sTran);
 		world.mulRotateZ(sRot.z);
-		a = world * oa;
-		b = world * ob;
+		sp = world * osp;
+		ep = world * oep;
 		//直線
 		if (operateObjSw == 1) {
 			input(lTran, lRot, moveSpeed, angSpeed);
 		}
 		world.translate(lTran);
 		world.mulRotateZ(lRot.z);
-		c = world * oc;
-		d = world * od;
+		p0 = world * op0;
+		p1 = world * op1;
 		//直線の垂線ベクトル
 		world.rotateZ(lRot.z);
 		vn = world * ovn;
 
 		//「線分a_b」が 線分c_dを含む「直線」と交わっているか
-		VECTOR v1 = a - lTran;
-		VECTOR v2 = b - lTran;
+		VECTOR v1 = sp - lTran;
+		VECTOR v2 = ep - lTran;
 		float d1 = dot(vn, v1);
 		float d2 = dot(vn, v2);
 		if (d1 * d2 <= 0){
 			//交点の座標（内分点）
 			float m = Abs(d1);
 			float n = Abs(d2);
-			ip = (a * n + b * m) / (m + n);
+			ip = (sp * n + ep * m) / (m + n);
 			lineColor = red;
 			dispPointFlag = true;
 		}
@@ -103,44 +107,45 @@ void gmain()
 			lineColor = gray;
 			dispPointFlag = false;
 		}
+
 		//描画
-		mathSegment(c, d, lineColor, 10);
-		mathSegment(a, b, COLOR(220, 220, 220), 10);
-		mathPoint(a, COLOR(255, 180, 180), 20);
-		mathPoint(b, COLOR(0, 255, 0), 20);
+		mathSegment(p0, p1, lineColor, 10);
+		mathSegment(sp, ep, white, 10);
+		mathPoint(sp, pink, 20);
+		mathPoint(ep, green, 20);
 		if (dispPointFlag) {
-			mathPoint(ip, COLOR(255, 255, 0), 20);
+			mathPoint(ip, yellow, 20);
 		}
 		//解説用ベクトル描画
 		if (isTrigger(KEY_C))++dispSegSw %= 6;
 		switch (dispSegSw) {
 		case 1:
-			mathSegment(lTran, lTran + v1, COLOR(255, 200, 200), 3);
+			mathSegment(lTran, lTran + v1, pink, 3);
 			break;
 		case 2:
-			mathSegment(lTran, lTran + v1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran, lTran + v2, COLOR(0, 255, 0), 3);
+			mathSegment(lTran, lTran + v1, pink, 3);
+			mathSegment(lTran, lTran + v2, green, 3);
 			break;
 		case 3:
-			mathSegment(lTran, lTran + v1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran, lTran + v2, COLOR(0, 255, 0), 3);
-			mathSegment(lTran, lTran + vn, COLOR(255, 255, 0), 3);
+			mathSegment(lTran, lTran + v1, pink, 3);
+			mathSegment(lTran, lTran + v2, green, 3);
+			mathSegment(lTran, lTran + vn, yellow, 3);
 			break;
 		case 4:
-			mathSegment(lTran, lTran + v1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran, lTran + v2, COLOR(0, 255, 0), 3);
-			mathSegment(lTran, lTran + vn, COLOR(255, 255, 0), 3);
-			mathSegment(lTran, lTran + vn * d1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran + v1, lTran + vn * d1, COLOR(255, 200, 200), 3);
+			mathSegment(lTran, lTran + v1, pink, 3);
+			mathSegment(lTran, lTran + v2, green, 3);
+			mathSegment(lTran, lTran + vn, yellow, 3);
+			mathSegment(lTran, lTran + vn * d1, pink, 3);
+			mathSegment(lTran + v1, lTran + vn * d1, pink, 3);
 			break;
 		case 5:
-			mathSegment(lTran, lTran + v1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran, lTran + v2, COLOR(0, 255, 0), 3);
-			mathSegment(lTran, lTran + vn, COLOR(255, 255, 0), 3);
-			mathSegment(lTran, lTran + vn * d1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran + v1, lTran + vn * d1, COLOR(255, 200, 200), 3);
-			mathSegment(lTran, lTran + vn * d2, COLOR(0, 255, 0), 3);
-			mathSegment(lTran + v2, lTran + vn * d2, COLOR(0, 255, 0), 3);
+			mathSegment(lTran, lTran + v1, pink, 3);
+			mathSegment(lTran, lTran + v2, green, 3);
+			mathSegment(lTran, lTran + vn, yellow, 3);
+			mathSegment(lTran, lTran + vn * d1, pink, 3);
+			mathSegment(lTran + v1, lTran + vn * d1, pink, 3);
+			mathSegment(lTran, lTran + vn * d2, green, 3);
+			mathSegment(lTran + v2, lTran + vn * d2, green, 3);
 		}
 		textSize(50);
 		text((let)"d1 * d2 = " + d1 * d2, 800, 60);
