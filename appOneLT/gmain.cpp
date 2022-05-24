6ed233b00987cc7d2ea0cd3255ba28d0fae0de81
Coding_Ocean <eijo524@gmail.com>
2022-05-24 13:15:59 +0900

    first commit

--- appOneLT/gmain.cpp
+++ appOneLT/gmain.cpp
@@ -1,3 +1,4 @@
+// 直線の式
 #include"libOne.h"
 
 void mathSegment(const VECTOR& s, const VECTOR& e,
@@ -18,8 +19,8 @@ void mathPoint(const VECTOR& p, const COLOR& c, float sWeight)
 void input(VECTOR& t, VECTOR& r, float moveSpeed, float angSpeed)
 {
 	if (isPress(KEY_SHIFT)) {
-		if (isPress(KEY_D)) { r.z += angSpeed; }
-		if (isPress(KEY_A)) { r.z += -angSpeed; }
+		if (isPress(KEY_D)) { r.z += -angSpeed; }
+		if (isPress(KEY_A)) { r.z += angSpeed; }
 	}
 	else {
 		if (isPress(KEY_D)) { t.x += moveSpeed; }
@@ -32,122 +33,100 @@ void input(VECTOR& t, VECTOR& r, float moveSpeed, float angSpeed)
 void gmain()
 {
 	window(1920, 1080, full);
-	//線分
-	VECTOR osp(0, 0.25f);
-	VECTOR oep(0, -0.25f);
-	VECTOR sp, ep;
-	VECTOR sTran(0.5f,0.5f), sRot;
 	//直線と垂線
-	VECTOR op0(1, 0);
-	VECTOR op1(-1, 0);
-	VECTOR ovn(0, 1);
-	VECTOR p0, p1, vn;
+	VECTOR osp(-1, -0.5f);
+	VECTOR oep= -osp;
+	VECTOR on(osp.y,-osp.x);
+	on.normalize();
+	VECTOR sp, ep, n;
 	VECTOR lTran, lRot;
-	//交点の座標
-	VECTOR ip;
-
+	//点ｐ
+	VECTOR op(0, 0);
+	VECTOR p;
+	VECTOR pTran, pRot;
+	//直線と点を移動回転させる共有データ
 	float moveSpeed = 0.006f;
 	float angSpeed = 0.01f;
 	MATRIX world;
-
-	bool dispPointFlag = false;
-	int dispSegSw = 0;
+	//移動、回転させる物の切換(0:直線、1:点)
 	int operateObjSw = 0;
-
+	//色
 	COLOR white(210, 210, 210);
 	COLOR pink(255, 128, 128);
 	COLOR green(0, 255, 0);
 	COLOR yellow(255, 255, 0);
 	COLOR lineColor;
 	COLOR gray(128, 128, 128);
-	COLOR red(255,0,0);
-
-	axisMode(NODRAW);
-	
+	COLOR red(255, 0, 0);
+	//メインループ
 	while (notQuit)
 	{
 		clear(0, 50, 0);
 		mathAxis(2.1f, 255);
 		//操作対象切換
 		if (isTrigger(KEY_Z))operateObjSw = 1 - operateObjSw;
-		//線分
-		if (operateObjSw == 0) {
-			input(sTran, sRot, moveSpeed, angSpeed);
-		}
-		world.translate(sTran);
-		world.mulRotateZ(sRot.z);
-		sp = world * osp;
-		ep = world * oep;
-		//直線
+		//点の移動----------------------------------------------------
 		if (operateObjSw == 1) {
+			input(pTran, pRot, moveSpeed, angSpeed);
+		}
+		world.translate(pTran);
+		p = world * op;
+		//直線の回転、移動----------------------------------------------
+		if (operateObjSw == 0) {
 			input(lTran, lRot, moveSpeed, angSpeed);
 		}
 		world.translate(lTran);
 		world.mulRotateZ(lRot.z);
-		p0 = world * op0;
-		p1 = world * op1;
-		//直線の垂線ベクトル
+		sp = world * osp;
+		ep = world * oep;
+		//直線の垂線ベクトルの回転
 		world.rotateZ(lRot.z);
-		vn = world * ovn;
+		n = world * on;
+		//直線の式を求めp.xからp.yを計算する-----------------------------
+		// 直線の式の一般形 
+		// ax + by + c = 0
+		// aは垂直ベクトルのｘ、bは垂直ベクトルのｙ、cは直線から原点までの最短距離
+		// cについて解くと
+		// c = -ax-by
+		// このプログラムの変数を式に当てはめｃを求める。
+		// x,y座標は直線上のどの１点でもよい。
+		// 垂直ベクトルと反対方向に原点があるとc<0となる
+		float c = -n.x * sp.x - n.y * sp.y;//これって内積だよねdot(-n,sp)
+		// 直線の式をyについて解くと
+		// y = ( -ax - c ) / b
+		// a,b,cがわかっているので点p.xからp.yを求めることができる
+		p.y = (-n.x * p.x - c) / n.y;
 
-		//「線分a_b」が 線分c_dを含む「直線」と交わっているか
-		VECTOR v1 = sp - lTran;
-		VECTOR v2 = ep - lTran;
-		float d1 = dot(vn, v1);
-		float d2 = dot(vn, v2);
-		if (d1 * d2 <= 0){
-			//交点の座標（内分点）
-			float m = Abs(d1);
-			float n = Abs(d2);
-			ip = (sp * n + ep * m) / (m + n);
-			lineColor = red;
-			dispPointFlag = true;
-		}
-		else {
-			lineColor = gray;
-			dispPointFlag = false;
-		}
+		//描画------------------------------------------------------------
+		//直線
+		mathSegment(sp, ep, lineColor, 10);
+		//垂線ベクトル
+		mathSegment(VECTOR(0, 0), n, lineColor, 1);
+		//cの見える化
+		mathSegment(VECTOR(0, 0), n * -c, yellow, 6);
+		//点ｐ
+		mathPoint(p, pink, 20);
 
-		//描画
-		mathSegment(p0, p1, lineColor, 10);
-		mathSegment(sp, ep, white, 10);
-		mathPoint(sp, pink, 20);
-		mathPoint(ep, green, 20);
-		if (dispPointFlag) {
-			mathPoint(ip, yellow, 20);
-		}
-		//解説用ベクトル描画
-		if (isTrigger(KEY_C))++dispSegSw %= 6;
-		switch (dispSegSw) {
-		case 1:
-			mathSegment(lTran, lTran + v1, pink, 3);
-			break;
-		case 2:
-			mathSegment(lTran, lTran + v1, pink, 3);
-			mathSegment(lTran, lTran + v2, green, 3);
-			break;
-		case 3:
-			mathSegment(lTran, lTran + v1, pink, 3);
-			mathSegment(lTran, lTran + v2, green, 3);
-			mathSegment(lTran, lTran + vn, yellow, 3);
-			break;
-		case 4:
-			mathSegment(lTran, lTran + v1, pink, 3);
-			mathSegment(lTran, lTran + v2, green, 3);
-			mathSegment(lTran, lTran + vn, yellow, 3);
-			mathSegment(lTran, lTran + vn * d1, pink, 3);
-			mathSegment(lTran + v1, lTran + vn * d1, pink, 3);
-			break;
-		case 5:
-			mathSegment(lTran, lTran + v1, pink, 3);
-			mathSegment(lTran, lTran + v2, green, 3);
-			mathSegment(lTran, lTran + vn, yellow, 3);
-			mathSegment(lTran, lTran + vn * d1, pink, 3);
-			mathSegment(lTran + v1, lTran + vn * d1, pink, 3);
-			mathSegment(lTran, lTran + vn * d2, green, 3);
-			mathSegment(lTran + v2, lTran + vn * d2, green, 3);
-		}
-		textSize(50);
-		text((let)"d1 * d2 = " + d1 * d2, 800, 60);
+		//ｃを求める式は逆垂線ベクトルとsｐとの内積と同じ
+		//mathSegment(VECTOR(0, 0), -n*dot(-n,sp), red, 3);
+		//mathSegment(VECTOR(0, 0), -n, red, 3);
+		//mathSegment(VECTOR(0, 0), sp, red, 3);
+
+		//テキスト情報
+		float size = 50;
+		float rowH = size + 10;
+		float colL = 10;
+		int num=0;
+		textSize(size);
+		if(operateObjSw==0)	
+			text((let)"直線の", colL, ++num*rowH);
+		else
+			text((let)" 点 の", colL, ++num * rowH);
+		if (isPress(KEY_SHIFT))
+			text((let)"回転 : shift + AD", colL + size * 3, num * rowH);
+		else
+			text((let)"移動 : ADWS", colL + size * 3, num * rowH);
+		text((let)"操作対象切換 : Z", colL, ++num * rowH);
+		text((let)"原点までの最短距離:" + c, colL, ++num*rowH);
 	}
 }
